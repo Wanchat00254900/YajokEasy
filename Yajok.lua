c62d27150fc5e8eaedc4b6d535c2be79420a20f6
Easy Hub <107304900+Wanchat00254900@users.noreply.github.com>
2022-11-27 16:24:11 +0700

    Update Yajok.lua

--- Yajok.lua
+++ Yajok.lua
@@ -1,18 +1,58 @@
-Bans = {
-"za0623247821"
-}
+local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+ 
+local win = DiscordLib:Window("System Hub | Yajok City")
+ 
+local serv = win:Server("Main", "rbxassetid://11247127403")
+ 
+local tgls = serv:Channel("Auto Farm")
+local tp = serv:Channel("Teleport")
+ 
+tgls:Toggle("Auto Vegetable - Need Knife",false, function(bool)
+_G.Vegetable = bool
 
-
-for i,v in pairs(game.Players:children()) do
-	
-	if not table.find(Bans,v.Name) then
-		
-	else
-	
-	v:Kick("You Are Banned")
-		
-	end
-	
+wait(0.2)
+while _G.Vegetable do wait(5.9)
+for i,v in pairs(game:GetService("Workspace").ServerMap.Vegetable:GetChildren()) do
+    if v.Name == "Vegetable" and v.Transparency == 0 then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame * CFrame.new(0,1,0)
+wait(0.2)
+game:GetService("VirtualInputManager"):SendKeyEvent(true,"E",false,game)
+task.wait(2)
+game:GetService("VirtualInputManager"):SendKeyEvent(false,"E",false,game)
+task.wait(2.7)
 end
+end
+end
+end)
+
+tp:Button("Hospital", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(435.64, 44.758, 252.18)
+end)
+
+tp:Button("Police Station", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(123.834, 40.395, 1439.277)
+end)
+
+tp:Button("Lebel", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2038.675, 47.217, 3403.504)
+end)
+
+tp:Button("Crafting Table", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2038.675, 47.217, 3403.504)
+end)
+
+tp:Button("Spawn", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(415.309, 39.961, 43.53)
+end)
 
 
+
+        spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.Vegetable then
+            end
+        end)
+       end)
+        end)
+    
